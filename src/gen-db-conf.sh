9f07a7c0e195e84d21e9c98cd0eb2c1c04928596
Jason Graham <jgraha8@gmail.com>
2013-09-28 21:02:25 -0400

    Added script for generating the turbdb config file for the TDB channel DB

---
+++ src/gen-db-conf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DB_ROOT="/datascope/tdbchannel"
+
+echo "${DB_ROOT}/grid.h5" > turbdb.conf
+
+dt=0.00136
+N_step_db=5;
+
+index=0;
+for ((i=1; i<=10; i++))
+do
+    for f in ${DB_ROOT}/tdb-chunk-$i/fields*.h5
+    do 
+	db_time=$(echo "scale=9; $((index-1))*${dt}*${N_step_db}" | bc)
+	echo $db_time $f >> turbdb.conf
+	((index++))
+    done
+done
